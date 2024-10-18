de2898027b7b483bba16ecd84df0c8ae02eef6f7
Ghoufrane <baloncesmeed@gmail.com>
2024-10-18 12:07:03 +0100

    AL-32 create method for accepte restaurants or refused

--- allofood-frontend/src/views/superadmin/Restaurant/index.jsx
+++ allofood-frontend/src/views/superadmin/Restaurant/index.jsx
@@ -41,6 +41,7 @@ const Orders = () => {
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
+              
               <th scope="col" className="px-6 py-3">Name</th>
               <th scope="col" className="px-6 py-3">Description</th>
               <th scope="col" className="px-6 py-3">Phone</th>
