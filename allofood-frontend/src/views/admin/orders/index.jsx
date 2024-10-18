3d2ba9f646e23968e515c28405ad8016d396fe75
unknown <rachidchaf2001@gmail.com>
2024-10-18 23:07:07 +0100

    added filter functionality to orders Task
    Added handleFilter function and the Filter component.

--- allofood-frontend/src/views/admin/orders/index.jsx
+++ allofood-frontend/src/views/admin/orders/index.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axiosInstance from '../../../api/config/axios';
 import OrderCard from './components';
+import FilterAndSearch from './components/filter';
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
+  const [filteredOrders, setFilteredOrders] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -15,6 +17,7 @@ const Orders = () => {
       setLoading(true);
       const response = await axiosInstance.get('/manager/orders');
       setOrders(response.data);
+      setFilteredOrders(response.data); // Initialize filtered orders
       setLoading(false);
     } catch (error) {
       console.error(error);
@@ -22,15 +25,27 @@ const Orders = () => {
     }
   };
 
+  const handleFilter = (filters) => {
+    const { status } = filters;
+    if (status) {
+      setFilteredOrders(orders.filter(order => order.status === status));
+    } else {
+      setFilteredOrders(orders);
+    }
+  };
+
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-2xl font-bold mb-6">Orders</h1>
+      <div className='flex justify-between'>
+        <h1 className="text-2xl font-bold mb-6">Orders</h1>
+        <FilterAndSearch onFilter={handleFilter} />
+      </div>
       {loading ? (
         <div className="text-center py-4">Loading...</div>
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {orders.map((order) => (
-            <OrderCard key={order._id} order={order} />          
+          {filteredOrders.map((order) => (
+            <OrderCard key={order._id} order={order} />
           ))}
         </div>
       )}
