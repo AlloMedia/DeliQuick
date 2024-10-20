93ee9274bdc80442154b259201d6d35089fdc334
unknown <rachidchaf2001@gmail.com>
2024-10-20 18:28:48 +0100

    UI enhancement added.
    updating status-data, resolving the status miss area by deleting uncessary classes in the model, edit and the edit modal files.

--- allofood-frontend/src/views/admin/orders/components/modal/edit.jsx
+++ allofood-frontend/src/views/admin/orders/components/modal/edit.jsx
@@ -47,7 +47,8 @@ const EditOrder = ({ isOpen, onRequestClose, order, onUpdateStatus }) => {
               className="mt-1 block w-full border pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
             >
               <option value="Pending">Pending</option>
-              <option value="Processing">Processing</option>
+              <option value="In Progress">In Progress</option>
+              <option value="Ready">Ready</option>
               <option value="Delivered">Delivered</option>
               <option value="Cancelled">Cancelled</option>
               <option value="Rejected">Rejected</option>
