5f418ff480b9e6dc8d91edc6b2764035b8b7c930
othman4dev <othmandevsup@gmail.com>
2024-10-21 10:24:16 +0100

    Adding the file superadminRoutes again

---
+++ allofood-backend/routes/superadminRoutes.js
@@ -0,0 +1,19 @@
+const superAdminController = require("../controllers/superAdmin/SuperAdminController");
+const RestaurantController = require("../controllers/superAdmin/RestaurantController");
+const express = require("express");
+const router = express.Router();
+
+router.post("/add", superAdminController.addRestaurant);
+
+// Route for rejecting or accepting a restaurant
+router.put(
+  "/restaurants/reject-or-accept/:restaurantId",
+  superAdminController.rejectOrAcceptRestaurant
+);
+router.get(
+  "/Restaurant/unapproved",
+  RestaurantController.getUnapprovedRestaurants
+);
+// router.delete('/restaurants/:restaurantId', superAdminController.deleteRestaurant);
+
+module.exports = router;
