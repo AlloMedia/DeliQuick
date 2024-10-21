a6d294ea5c58de640c0780c7a4c2b3de2afe675a
Nada Elmahfoudi <elmahfoudinada17@gmail.com>
2024-10-21 15:20:49 +0100

    AL-36 Adding backend logic for searching restaurants

--- allofood-backend/routes/superadminRoutes.js
+++ allofood-backend/routes/superadminRoutes.js
@@ -4,6 +4,8 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/add", superAdminController.addRestaurant);
+router.put("/edit/:restaurantId", superAdminController.editRestaurant);
+router.get("/search", superAdminController.searchRestaurants);
 
 // Route for rejecting or accepting a restaurant
 router.put(
