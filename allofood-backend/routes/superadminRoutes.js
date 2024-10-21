e64c02624cc8f75a60832e252df74a4220b08efb
othman4dev <othmandevsup@gmail.com>
2024-10-21 10:05:04 +0100

    Adding routes of superAdminController after changing manully from the remote

--- allofood-backend/routes/superadminRoutes.js
+++ allofood-backend/routes/superadminRoutes.js
@@ -1,7 +1,18 @@
-const superAdminController = require("../controllers/superAdmin/superAdminController");
 const express = require("express");
+const {
+  rejectOrAcceptRestaurant,
+} = require("../controllers/superAdmin/SuperAdminController");
+const {
+  getUnapprovedRestaurants,
+} = require("../controllers/superAdmin/RestaurantController");
 const router = express.Router();
 
+// Route for rejecting or accepting a restaurant
+router.put(
+  "/restaurants/reject-or-accept/:restaurantId",
+  rejectOrAcceptRestaurant
+);
+router.get("/Restaurant/unapproved", getUnapprovedRestaurants);
 router.post("/add", superAdminController.addRestaurant);
 
 module.exports = router;
