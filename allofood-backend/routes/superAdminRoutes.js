df552b1ed322b18286ea0d74377faf490277d01b
unknown <rachidchaf2001@gmail.com>
2024-10-22 12:45:23 +0100

    added delivery confirm task.
    updating the delivery Routes and DeliveryController.

--- allofood-backend/routes/superAdminRoutes.js
+++
@@ -1,7 +0,0 @@
-const superAdminController = require("../controllers/superAdmin/superAdminController");
-const express = require("express");
-const router = express.Router();
-
-router.post("/add", superAdminController.addRestaurant);
-
-module.exports = router;
