1b74fb73083baa3205601648833fd37e300b7c29
unknown <rachidchaf2001@gmail.com>
2024-10-22 12:10:38 +0100

    added delivery confirm task.
    updating the delivery Routes and DeliveryController.

--- allofood-backend/routes/deliveryRoutes.js
+++ allofood-backend/routes/deliveryRoutes.js
@@ -3,6 +3,7 @@ const deliveryController = require("../controllers/delivery/DeliveryController")
 const express = require("express");
 const router = express.Router();
 
+router.post("/accept", deliveryController.acceptDelivery);
 router.get("/me", [''], deliveryController.getDelivery);
 
 module.exports = router;
