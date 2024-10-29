c1bbce5f84fd355e3b063c00f80a912f2f5d2ffb
Nada Elmahfoudi <elmahfoudinada17@gmail.com>
2024-10-29 09:27:49 +0100

    AL-34 Add Backend Logic to add item to cart and getUserCart Functionnalities with routes

--- allofood-backend/routes/clientRoutes.js
+++ allofood-backend/routes/clientRoutes.js
@@ -5,5 +5,6 @@ const clientController = require('../controllers/client/ClientController');
 
 // Route to add an item to the cart
 router.post('/add', clientController.addItemToCart);
+router.get('/cart/:userId', clientController.getUserCart);
 
 module.exports = router;
